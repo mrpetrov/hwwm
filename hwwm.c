390031667f39d30c5ab2a52ffffc754d6a64b1a8
mrpetrov <mrpetrov@users.noreply.github.com>
2020-10-26 19:01:13 +0200

    hwwm: lower minimum runtime for electrical heater
    it used to be 10 mins minimum, now lower it to 8 mins

--- hwwm.c
+++ hwwm.c
@@ -1324,7 +1324,7 @@ unsigned short CanTurnHeaterOn() {
 }
 
 unsigned short CanTurnHeaterOff() {
-    if ((CHeater) && (SCHeater > 59)) return 1;
+    if ((CHeater) && (SCHeater > 48)) return 1;
     else return 0;
 }
 
