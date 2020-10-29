41ede4e6276d4eaed395f54b5e8d41157a163f7f
mrpetrov <mrpetrov@users.noreply.github.com>
2020-10-29 14:13:47 +0200

    update-hwwm-executable script: rework things a bit
    No need to use systemd units to manage hwwm lifecycle, so go manual a bit - script out what simple steps need to be done to stop hwwm if running, replace the binary at /usr/sbin, and start it again

--- scripts/update-hwwm-executable.sh
+++ scripts/update-hwwm-executable.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-service_name=hwwm
-src_dir=/home/pi/$service_name
 
-service $service_name stop
-cp $src_dir/$service_name /usr/bin
-chown root:root /usr/bin/$service_name
-chmod a+x /usr/bin/$service_name
-sleep 6
-service $service_name start
+daemon=hwwm
+daemon_pid=/run/$daemon.pid
 
+echo "Trying to kill running daemon $daemon..."
+if [ ! -e $daemon_pid ]
+then
+    killall -e $daemon
+else
+    kill `cat $daemon_pid`
+    rm -f $daemon_pid
+fi
+sleep 1
+echo "Replacing /usr/sbin/$daemon with the one from /home/pi/$daemon..."
+cp /home/pi/$daemon/$daemon /usr/sbin
+sleep 3
+echo "Starting $daemon again..."
+/usr/sbin/$daemon
