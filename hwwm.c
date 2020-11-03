8904d929c49ad505ca861f26ff0dade1835565fa
mrpetrov <mrpetrov@users.noreply.github.com>
2020-11-03 20:48:54 +0200

    hwwm: keep pump a bit longer on right after Heat Pump has just turned off

--- hwwm.c
+++ hwwm.c
@@ -1510,6 +1510,8 @@ ComputeWantedState() {
     if ((Tkotel > 20)&&(Tkotel > (TkotelPrev+0.12))) wantP1on = 1;
     /* Furnace temp is rising QUICKLY - turn pump on to limit furnace thermal shock */
     if (Tkotel > (TkotelPrev+0.18)) wantP1on = 1;
+    /* If Heat Pump has recently turned off, keep pump on for a bit longer */
+    if (!CHP_low && (SCHP_low<7)) wantP1on = 1;
     /* Cycle furnace water every 7 minutes that Heat Pump has been OFF */
     if (!CHP_low && ((SCHP_low%42)==0)) wantP1on = 1;
 
