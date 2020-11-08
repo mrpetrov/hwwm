810dd08ce42055931c9d2c62d97bb82fd522bf34
mrpetrov <mrpetrov@users.noreply.github.com>
2020-11-08 10:42:20 +0200

    scripts: add hwwm-reload, hwwm-restart, and make update-hwwm script place them in /usr/sbin

--- scripts/update-hwwm-executable.sh
+++ scripts/update-hwwm-executable.sh
@@ -12,11 +12,14 @@ else
     kill `cat $daemon_pid`
     rm -f $daemon_pid
 fi
-sleep 1
+sleep 5
 echo "Replacing /usr/sbin/$daemon with the one from /home/pi/$daemon..."
 cp /home/pi/$daemon/$daemon /usr/sbin
-sleep 3
+echo "Replacing $daemon-reload and $daemon-restart in /usr/sbin with ones from /home/pi/$daemon/scripts/..."
+cp /home/pi/$daemon/scripts/$daemon-reload /usr/sbin
+cp /home/pi/$daemon/scripts/$daemon-restart /usr/sbin
+sleep 5
 echo "Starting $daemon again..."
 /usr/sbin/$daemon
 echo "Here is the log:"
-tail -n 25 $log_file
+tail -n 30 $log_file
