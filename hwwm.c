5d71f1b8210476552900ba18fe7037e61d1485ad
mrpetrov <mrpetrov@users.noreply.github.com>
2021-12-18 19:12:49 +0200

    if furnace is runnig hot - save some electrical energy

--- hwwm.c
+++ hwwm.c
@@ -1519,9 +1519,12 @@ BoilerNeedsHeat() {
     short ret = 0;
     /* if both day and night use of electric heater are disabled - return 0 */
     if (!cfg.use_electric_heater_night && !cfg.use_electric_heater_day) return 0;
-    if ( TboilerHigh < ((float)cfg.wanted_T) ) ret+=1;
-    if ( TboilerLow < ((float)cfg.wanted_T - ((TenvAvrg < 16) ? 3:11)) ) ret+=20;
-    if ( CHeater && CHP_low && (TboilerLow < ((float)cfg.wanted_T)) ) ret+=300;
+    if ( Tkotel <  ((float)cfg.wanted_T + 6) )
+    {
+        if ( TboilerHigh < ((float)cfg.wanted_T) ) ret+=1;
+        if ( TboilerLow < ((float)cfg.wanted_T - ((TenvAvrg < 16) ? 3:11)) ) ret+=20;
+        if ( CHeater && CHP_low && (TboilerLow < ((float)cfg.wanted_T)) ) ret+=300;
+    }
     return ret;
 }
 
