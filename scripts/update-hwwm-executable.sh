faf3ad48750fe3949403e6840d0faef90d737a4d
mrpetrov <mrpetrov@users.noreply.github.com>
2022-11-20 16:50:49 +0200

    Update hwwm scripts - add one to do backup

--- scripts/update-hwwm-executable.sh
+++ scripts/update-hwwm-executable.sh
@@ -18,8 +18,12 @@ cp /home/pi/$daemon/$daemon /usr/sbin
 echo "Replacing $daemon-reload and $daemon-restart in /usr/sbin with ones from /home/pi/$daemon/scripts/..."
 cp /home/pi/$daemon/scripts/$daemon-reload /usr/sbin
 cp /home/pi/$daemon/scripts/$daemon-restart /usr/sbin
+cp /home/pi/$daemon/scripts/$daemon-stop /usr/sbin
+cp /home/pi/$daemon/scripts/hwwm_backup-cfg /usr/sbin
 chmod +x /usr/sbin/$daemon-reload
 chmod +x /usr/sbin/$daemon-restart
+chmod +x /usr/sbin/$daemon-stop
+chmod +x /usr/sbin/hwwm_backup-cfg
 sleep 3
 echo "Starting $daemon again..."
 /usr/sbin/$daemon
